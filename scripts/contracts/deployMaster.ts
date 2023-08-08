25da04f683b3891760a241f834efca78ff89f5a4
Joseph Chalabi <chalabi.joseph@gmail.com>
2023-08-07 23:05:01 -0700

    Add canto local net

--- scripts/contracts/deployMaster.ts
+++ scripts/contracts/deployMaster.ts
@@ -1,12 +1,12 @@
 import { ethers } from "hardhat";
 
-export async function DeployMaster() {
+ export async function DeployMaster() {
 
     const [deployer] = await ethers.getSigners();
 
     console.log("Deploying RefundMaster with the account:", deployer.address);
 
-    const TREASURY_ADDRESS = "0x98d1f7347FF1d0984b6daB1E1Ae3C693d2b0B380";
+    const TREASURY_ADDRESS = "0x98d1f7347FF1d0984b6daB1E1Ae3C693d2b0B380" //currently set to the public key i used;
     const RefundMaster = await ethers.getContractFactory("RefundMaster");
     const masterContract = await RefundMaster.deploy(TREASURY_ADDRESS); 
 
--- scripts/test.ts
+++ scripts/test.ts
@@ -1,28 +1,39 @@
 import { waffle, ethers } from "hardhat";
-import { DeployMaster } from "../scripts/contracts/deployMaster";
-import { SendEth } from "./tx/sendEth";
-import { DeployChildren } from "./contracts/deployChildren";
-import { BatchQuery } from "./tests/batchQuery";
-import { CallRefund } from "./contracts/callRefund";
 import { QueryWallet } from "./tests/queryWallets";
 import { expect } from "chai";
+import { DeployMaster } from "./contracts/deployMaster";
+import { CallRefund } from "./contracts/callRefund";
+import { DeployChildren } from "./contracts/deployChildren";
+import { BatchQuery } from "./tests/batchQuery";
+import { SendEth } from "./tx/sendEth";
 
 describe("Full Integration Test", function() {
     it("Should execute all scripts in sequence", async function() {
-        // This assumes each function is asynchronous and that they return 
-        // any necessary data for assertions or for the next function.
+        try {
 
-        await DeployMaster();
-        await SendEth();
-        await DeployChildren();
-        await BatchQuery();
-        await CallRefund();
-        
-        // Capture the returned data from QueryWallet
-        const queryWalletResults = await QueryWallet();
+            await DeployMaster();
+            await SendEth();
+            await DeployChildren();
+            await BatchQuery();
+            await CallRefund();
+            
+            // Capture the returned data from QueryWallet
+            const queryWalletResults = await QueryWallet();
 
-        // Now make assertions based on the returned results:
-        expect(queryWalletResults.totalETH).to.equal(queryWalletResults.expectedETH);
-        expect(queryWalletResults.walletsMismatched).to.equal(0);
+            expect(queryWalletResults.totalETH).to.equal(queryWalletResults.expectedETH);
+            expect(queryWalletResults.walletsMismatched).to.equal(0);
+
+            // Log the results
+            console.log("Results:");
+            console.log("Total ETH:", queryWalletResults.totalETH);
+            console.log("Expected ETH:", queryWalletResults.expectedETH);
+            console.log("Mismatched Wallets:", queryWalletResults.walletsMismatched);
+
+            console.log("Test Passed Successfully!");
+
+        } catch (error) {
+            console.error("Error occurred:", error);
+            throw error;
+        }
     });
 });
